b17c0d0b9e6219fd309cd36520c5442e6170474a
Jo-Chang <74034844+Jo-Chang@users.noreply.github.com>
2023-04-27 17:04:07 +0900

    Update Store model, Add store CRUD

--- stores/forms.py
+++ stores/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from .models import Store
 
+
 class StoreForm(forms.ModelForm):
     class Meta:
         model = Store
-        fields = '__all__'
+        exclude = ('like_users', )
