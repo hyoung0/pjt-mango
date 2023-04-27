07c24f3fa9ab45934f8e597b0ea66618acca0001
최수현 <sw92522@naver.com>
2023-04-27 15:25:57 +0900

    Create models of reviews

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.conf import settings
+from ..stores import models
 
 # Create your models here.
+class Review(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
