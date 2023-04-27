6bb39a6a5e883b877690994602cf6ecb4a36f713
최수현 <sw92522@naver.com>
2023-04-28 01:59:26 +0900

    Create structure of reviews

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from . import views
 
 
+app_name = 'reviews'
 urlpatterns = [
+    path('<int:store_pk>/create/', views.create, name='create'),
+    path('<int:review_pk>/update/', views.update, name='update'),
+    path('<int:review_pk>/delete/', views.delete, name='delete'),
+    path('<int:review_pk>/emotes/', views.emotes, name='emotes'),
 ]
