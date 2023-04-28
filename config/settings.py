4789b6bc0362acd4632642b354905b019cbb0eac
hyoung0 <choyoung1034@naver.com>
2023-04-28 10:58:48 +0900

    add allowedhost

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['port-0-pjt-mango-e9btb72mlgzw4suv.sel4.cloudtype.app']
 
 
 # Application definition
