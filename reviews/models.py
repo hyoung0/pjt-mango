499d48b842d664dbddbc2d3c32a4fcefdbebd6e7
최수현 <sw92522@naver.com>
2023-04-28 16:38:40 +0900

    Add reviews content to stores detail

--- reviews/models.py
+++ reviews/models.py
@@ -1,17 +1,30 @@
 from django.db import models
 from django.conf import settings
 from stores.models import Store
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 # Create your models here.
 class Review(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_reviews')
+    emote_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='emote_reviews', through='Emote')
     content = models.CharField(max_length=500)
     rating = models.IntegerField()
-    image = models.ImageField(blank=True, null=True)
+
+    def reviews_image_path(instance, filename):
+        return f'reviews/{instance.user.username}/{filename}'
+
+    image = ProcessedImageField(upload_to='reviews_image_path', blank=True, null=True,
+                                processors=[ResizeToFill(100,100)],
+                                format='jpg',
+                                options={'quality': 80})
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
+
+    def rate_to_star(self):
+        return '★' * self.rating
 
 
 class Emote(models.Model):
