cde42be4b61b518065e470eb51230af19725d859
최수현 <sw92522@naver.com>
2023-05-02 03:37:36 +0900

    Update reviews form, update logic, model function

--- reviews/models.py
+++ reviews/models.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from stores.models import Store
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
+from datetime import datetime, timedelta
+from django.utils import timezone
 
 # Create your models here.
 class Review(models.Model):
@@ -12,18 +14,36 @@ class Review(models.Model):
     content = models.CharField(max_length=500)
     rating = models.IntegerField()
 
+    def rate_to_star(self):
+        return '★' * self.rating
+
     def reviews_image_path(instance, filename):
         return f'reviews/{instance.user.username}/{filename}'
 
-    image = ProcessedImageField(upload_to='reviews_image_path', blank=True, null=True,
+    image = ProcessedImageField(upload_to=reviews_image_path, blank=True, null=True,
                                 processors=[ResizeToFill(100,100)],
                                 format='JPEG',
                                 options={'quality': 80})
-    created_at = models.DateField(auto_now_add=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def created_string(self):
+        time = datetime.now(tz=timezone.utc) - self.created_at
+        if time < timedelta(minutes=1):
+            return '방금 전'
+        elif time < timedelta(hours=1):
+            return str(time.seconds // 60) + '분 전'
+        elif time < timedelta(days=1):
+            return str(time.seconds // 3600) + '시간 전'
+        elif time < timedelta(weeks=1):
+            time = datetime.now(tz=timezone.utc).date() - self.created_at.date()
+            return str(time.days) + '일 전'
+        else:
+            return self.created_at.strftime('%Y-%m-%d')
+        
     updated_at = models.DateField(auto_now=True)
 
-    def rate_to_star(self):
-        return '★' * self.rating
 
 
 class Emote(models.Model):
